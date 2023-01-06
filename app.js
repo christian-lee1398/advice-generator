3c3b70b917aaede7c65350358f9dcbf451af1100
Christian Lee <christian.lee1398@gmail.com>
2023-01-05 22:59:07 -0500

    get request from api

--- app.js
+++ app.js
@@ -6,27 +6,31 @@
 //         console.log("Error", err);
 //     })
 
-const adviceID = document.querySelector("advice-id")
-const advice = document.querySelector("advice")
-const adviceDice = document.querySelector("advice-dice")
+const adviceID = document.querySelector(".advice-id")
+const advice = document.querySelector(".advice")
+const adviceDice = document.querySelector(".dice-button")
 
-// adviceDice.addEventListener('click', newAdvice)
-
-const newAdvice = () => {
-    const adviceText = getAdvice();
+const newAdvice = async () => {
+    const adviceText = await getAdvice();
     console.log(adviceText)
     // adviceID.innerHTML += `${res.data.slip}`
+
 }
 
 const getAdvice = async () => {
     const res = await axios.get("https://api.adviceslip.com/advice")
-        .then(res => {
-            console.log(res.data.slip)
-        })
-        .catch(err => {
-            console.log("Error", err);
-        })
+    // .then(res => {
+    //     console.log(res.data.slip)
+    // })
+    // .catch(err => {
+    //     console.log("Error", err);
+    // })
+    adviceID.innerHTML = `Advice #${res.data.slip.id}`;
+    advice.innerHTML = `${res.data.slip.advice}`;
+    return res.data.slip;
+
 }
+adviceDice.addEventListener('click', newAdvice)
 
 // function log(msg) {
 //     const logElem = document.querySelector(".log");
